fadf627dd38b0328e0720b3a8c7884f954711283
paul <abbb123@naver.com>
2025-12-28 11:19:41 +0900

    feat: clerk 한글화 적용

--- src/services/clerk/components/ClerkProvider.tsx
+++ src/services/clerk/components/ClerkProvider.tsx
@@ -1,6 +1,11 @@
 import { ReactNode } from "react";
 import { ClerkProvider as OriginalClerkProvider } from "@clerk/nextjs";
+import { koKR } from "@clerk/localizations";
 
 export function ClerkProvider({ children }: { children: ReactNode }) {
-  return <OriginalClerkProvider>{children}</OriginalClerkProvider>;
+  return (
+    <OriginalClerkProvider localization={koKR}>
+      {children}
+    </OriginalClerkProvider>
+  );
 }
