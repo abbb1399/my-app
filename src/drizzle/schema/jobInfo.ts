36ebfcaa3a6e12375e39f93edfa617ea36e8e83b
paul <abbb123@naver.com>
2026-01-01 18:53:16 +0900

    feat: interview, jobinfo, question 테이블 정의

---
+++ src/drizzle/schema/jobInfo.ts
@@ -0,0 +1,36 @@
+import { pgEnum, pgTable, varchar } from "drizzle-orm/pg-core";
+import { createdAt, id, updatedAt } from "@/drizzle/schemaHelpers";
+import { UserTable } from "./user";
+import { relations } from "drizzle-orm";
+import { InterviewTable } from "./interview";
+import { QuestionTable } from "./question";
+
+export const experienceLevels = ["junior", "mid-level", "senior"] as const;
+export type ExperienceLevel = (typeof experienceLevels)[number];
+
+export const ExperienceLevelEnum = pgEnum(
+  "job_infos_experience_level",
+  experienceLevels
+);
+
+export const JobInfoTable = pgTable("job_info", {
+  id,
+  title: varchar().notNull(),
+  name: varchar().notNull(),
+  experienceLevel: ExperienceLevelEnum().notNull(),
+  description: varchar().notNull(),
+  userId: varchar()
+    .references(() => UserTable.id, { onDelete: "cascade" })
+    .notNull(),
+  createdAt,
+  updatedAt,
+});
+
+export const jobInfoRelations = relations(JobInfoTable, ({ one, many }) => ({
+  user: one(UserTable, {
+    fields: [JobInfoTable.userId],
+    references: [UserTable.id],
+  }),
+  questions: many(QuestionTable),
+  interviews: many(InterviewTable),
+}));
