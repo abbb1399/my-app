9c80d3d6921d7ebfa460fb583da4849ecb0c24bd
paul <abbb123@naver.com>
2026-01-03 22:08:26 +0900

    feat: 직무 Cards

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,12 +1,22 @@
-import { Card, CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { db } from "@/drizzle/db";
 import { JobInfoTable } from "@/drizzle/schema";
 import { JobInfoForm } from "@/features/jobInfos/components/JobInfoForm";
 import { getJobInfoUserTag } from "@/features/jobInfos/dbCache";
+import { formatExperienceLevel } from "@/features/jobInfos/lib/formatters";
 import { getCurrentUser } from "@/services/clerk/lib/getCurrentUser";
 import { desc, eq } from "drizzle-orm";
-import { Loader2 } from "lucide-react";
-import { cacheTag } from "next/cache";
+import { ArrowRightIcon, Loader2, PlusIcon } from "lucide-react";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default function AppPage() {
@@ -33,6 +43,63 @@ async function JobInfos() {
   if (jobInfos.length === 0) {
     return <NoJobInfos />;
   }
+
+  return (
+    <div className="container my-4">
+      <div className="flex gap-2 justify-between mb-6">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl">나의 직무들</h1>
+        <Button asChild>
+          <Link href="/app/job-infos/new">
+            <PlusIcon />
+            직무 생성하기
+          </Link>
+        </Button>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 has-hover:*:not-hover:opacity-70">
+        {jobInfos.map((jobInfo) => (
+          <Link
+            className="hover:scale-[1.02] transition-[transform_opacity]"
+            href={`/app/job-infos/${jobInfo.id}`}
+            key={jobInfo.id}
+          >
+            <Card className="h-full">
+              <div className="flex items-center justify-between h-full">
+                <div className="space-y-4 h-full">
+                  <CardHeader>
+                    <CardTitle className="text-lg">{jobInfo.name}</CardTitle>
+                  </CardHeader>
+                  <CardContent className="text-muted-foreground line-clamp-3">
+                    {jobInfo.description}
+                  </CardContent>
+                  <CardFooter className="flex gap-2">
+                    <Badge variant="outline">
+                      {formatExperienceLevel(jobInfo.experienceLevel)}
+                    </Badge>
+                    {jobInfo.title && (
+                      <Badge variant="outline">{jobInfo.title}</Badge>
+                    )}
+                  </CardFooter>
+                </div>
+                <CardContent>
+                  <ArrowRightIcon className="size-6" />
+                </CardContent>
+              </div>
+            </Card>
+          </Link>
+        ))}
+
+        <Link className="transition-opacity" href="/app/job-infos/new">
+          <Card className="h-full flex items-center justify-center border-dashed border-3 bg-transparent hover:border-primary/50 transition-colors shadow-none">
+            <div className="text-lg flex items-center gap-2">
+              <PlusIcon className="size-6" />
+              직무 생성하기
+            </div>
+          </Card>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 function NoJobInfos() {
