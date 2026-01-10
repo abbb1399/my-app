8aa5a5b883ac2bdd6344a73f0435b50b41bdd983
paul <abbb123@naver.com>
2026-01-10 19:02:09 +0900

    feat: JobInfo 수정 페이지

---
+++ src/features/jobInfos/components/JobInfoBackLink.tsx
@@ -0,0 +1,41 @@
+import { BackLink } from "@/components/BackLink";
+import { db } from "@/drizzle/db";
+import { JobInfoTable } from "@/drizzle/schema";
+import { cn } from "@/lib/utils";
+import { eq } from "drizzle-orm";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { Suspense } from "react";
+import { getJobInfoIdTag } from "../dbCache";
+
+export function JobInfoBackLink({
+  jobInfoId,
+  className,
+}: {
+  jobInfoId: string;
+  className?: string;
+}) {
+  return (
+    <BackLink
+      href={`/app/job-infos/${jobInfoId}`}
+      className={cn("mb-4", className)}
+    >
+      <Suspense fallback={"업무 설명"}>
+        <JobName jobInfoId={jobInfoId} />
+      </Suspense>
+    </BackLink>
+  );
+}
+
+async function JobName({ jobInfoId }: { jobInfoId: string }) {
+  const jobInfo = await getJobInfo(jobInfoId);
+  return jobInfo?.name ?? "업무 설명";
+}
+
+async function getJobInfo(id: string) {
+  "use cache";
+  cacheTag(getJobInfoIdTag(id));
+
+  return db.query.JobInfoTable.findFirst({
+    where: eq(JobInfoTable.id, id),
+  });
+}
