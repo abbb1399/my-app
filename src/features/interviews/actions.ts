b5fb428fc8e588b369f55b52b40a7f0576a319b9
paul <abbb123@naver.com>
2026-01-14 23:13:57 +0900

    feat: Interview 생성, 수정

---
+++ src/features/interviews/actions.ts
@@ -0,0 +1,97 @@
+"use server";
+
+import { getCurrentUser } from "@/services/clerk/lib/getCurrentUser";
+import { db } from "@/drizzle/db";
+import { InterviewTable, JobInfoTable } from "@/drizzle/schema";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { getJobInfoIdTag } from "@/features/jobInfos/dbCache";
+import { and, eq } from "drizzle-orm";
+import { insertInterview, updateInterview as updateInterviewDb } from "./db";
+import { getInterviewIdTag } from "./dbCache";
+import { get } from "http";
+
+export async function createInterview({
+  jobInfoId,
+}: {
+  jobInfoId: string;
+}): Promise<{ error: true; message: string } | { error: false; id: string }> {
+  const { userId } = await getCurrentUser();
+
+  if (userId == null) {
+    return {
+      error: true,
+      message: "권한이 없습니다.",
+    };
+  }
+
+  const jobInfo = await getJobInfo(jobInfoId, userId);
+
+  if (jobInfo == null) {
+    return {
+      error: true,
+      message: "권한이 없습니다.",
+    };
+  }
+
+  const interview = await insertInterview({
+    jobInfoId,
+    duration: "00:00:00",
+  });
+
+  return { error: false, id: interview.id };
+}
+
+export async function updateInterview(
+  id: string,
+  data: { humeChatId?: string; duration?: string }
+) {
+  const { userId } = await getCurrentUser();
+
+  if (userId == null) {
+    return {
+      error: true,
+      message: "권한이 없습니다.",
+    };
+  }
+
+  const interview = await getInterview(id, userId);
+
+  if (interview == null) {
+    return {
+      error: true,
+      message: "권한이 없습니다.",
+    };
+  }
+
+  await updateInterviewDb(id, data);
+
+  return { error: false };
+}
+
+async function getJobInfo(id: string, userId: string) {
+  "use cache";
+  cacheTag(getJobInfoIdTag(id));
+
+  return db.query.JobInfoTable.findFirst({
+    where: and(eq(JobInfoTable.id, id), eq(JobInfoTable.userId, userId)),
+  });
+}
+
+async function getInterview(id: string, userId: string) {
+  "use cache";
+  cacheTag(getInterviewIdTag(id));
+
+  const interview = await db.query.InterviewTable.findFirst({
+    where: eq(InterviewTable.id, id),
+    with: {
+      jobInfo: { columns: { id: true, userId: true } },
+    },
+  });
+
+  if (interview == null) return null;
+
+  cacheTag(getJobInfoIdTag(interview.jobInfo.id));
+  if (interview.jobInfo.userId !== userId) return null;
+
+  return interview;
+}
